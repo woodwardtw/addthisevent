43409f9e3197261940668063d30295063887ab19
Tom Woodward <twwoodward@me.com>
2015-03-20 08:00:27 -0400

    general improvements
    added CSS stylesheet
    added settings js
    improved registration/enqueing of scripts/stylesheet
    
    Next up will be a preference panel

--- addthisevent.php
+++ addthisevent.php
@@ -22,13 +22,39 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
-
-
-function add_addthisevent() {
-    wp_enqueue_script( 'add_this_event', 
+ 
+ //register  scripts
+function ate_scripts() {
+    wp_register_script( 'add_this_event', 
                        'https://addthisevent.com/libs/1.5.8/ate.min.js', 
                        array(),
-                       'scriptversion 1.0', 
+                       'scriptversion 1.5.8', 
                        true);
+    wp_register_script( 'add_this_events_prefs',
+                        plugins_url( '/js/prefs.js', __FILE__ ),
+                        array(add_this_event),
+                        '1',
+                        true);
+    wp_register_style('add_this_event_css',
+                        plugins_url( '/ate.css', __FILE__ ),
+                        array(),
+                        '1',
+                        false);
+//enque scripts
+    wp_enqueue_script('add_this_event');
+    wp_enqueue_script('add_this_events_prefs');
+    wp_enqueue_style('add_this_event_css');
+ 
+    $ate_license      = (get_option('ate_license') == '') ? "" : get_option('fwds_effect');
+    $ate_mouse    = (get_option('ate_mouse') == 'enabled') ? true : false;
+    $ate_css    = (get_option('ate_css') == 'enabled') ? true : false;
+    $config_array = array(
+            'license' => $ate_license,
+            'mouse' => $ate_mouse,
+            'css' => $ate_css
+        );
+ 
+    wp_localize_script('add_this_event_prefs', 'setting', $config_array);
+ 
 }
-add_action( 'wp_enqueue_scripts', 'add_addthisevent' );
+add_action( 'wp_enqueue_scripts', 'ate_scripts' );
